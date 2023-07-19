4442dc562d0f55bbc18d9352820c5980e27cc77b
Kushwanth3161 <121281594+Kushwanth3161@users.noreply.github.com>
2023-07-19 12:28:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function c1(){
+    document.getElementById("thala").src="https://s.abcnews.com/images/GMA/HT_henry_cavill_cf_160324_hpMain_7_4x5_608.jpg";
+    }
+    function c2(){
+    document.getElementById("thala").src="https://nationaltoday.com/wp-content/uploads/2022/05/63-Superman-Day.jpg";
+    }
+    function c3(){
+        document.getElementById("thala").src="https://media.gq.com/photos/639b4784d75c53023c32cf23/1:1/w_1996,h_1996,c_limit/MCDMAOF_EC475.jpeg";
+    }
